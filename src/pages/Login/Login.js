7a86731bb6c01c5fea38ef6ffc09c9cc16f88d80
syedmhdm <116684611+syedmhdm@users.noreply.github.com>
2023-02-12 15:28:50 +0530

    completed signup design and some validation

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -1,63 +1,86 @@
 import { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import useBearStore from "../../state/State";
+import styles from "./Login.module.css";
 
 const Login = (props) => {
   const [userLoginData, setUserLoginData] = useState({
     username: "",
     password: "",
   });
+  const [isDisabled, setIsDisabled] = useState(true);
+  const [errMsg, setErrMsg] = useState("");
   const navigate = useNavigate();
   const setIsUserValid = useBearStore((state) => state.setIsUserValid);
 
   const submitHandler = async (e) => {
     e.preventDefault();
 
-    const dbUsername = "syed";
-    const dbPassword = "admin";
+    // const dbUsername = "syed";
+    // const dbPassword = "admin";
 
-    if (
-      userLoginData.username === dbUsername &&
-      userLoginData.password === dbPassword
-    ) {
-      setIsUserValid(true);
-      navigate("/myprofile");
-    }
+    console.log("login data", userLoginData);
+
+    // if (
+    //   userLoginData.username === dbUsername &&
+    //   userLoginData.password === dbPassword
+    // ) {
+    //   setIsUserValid(true);
+    //   navigate("/myprofile");
+    // }
   };
 
   const onChange = (e) => {
     setUserLoginData((prev) => {
       let helper = { ...prev };
-      helper[`${e.target.id}`] = e.target.value;
+      helper[`${e.target.id}`] = e.target.value.replaceAll(" ", "");
+
+      if (helper.username && helper.password) {
+        setIsDisabled(false);
+      } else {
+        setIsDisabled(true);
+      }
+
       return helper;
     });
   };
 
   return (
-    <form onSubmit={submitHandler}>
-      <h4>Login</h4>
-      <input
-        onChange={onChange}
-        value={userLoginData.username}
-        name='username'
-        placeholder='username'
-        id='username'
-        type={"text"}
-      />
-      <input
-        onChange={onChange}
-        value={userLoginData.password}
-        name='password'
-        placeholder='password'
-        id='password'
-        type={"text"}
-      />
-      <button>submit</button>
+    <div className={styles["main"]}>
+      <form onSubmit={submitHandler} className={styles["form"]}>
+        <div className={styles["user-image"]}>
+          <div className={styles["head"]} />
+          <div className={styles["body"]} />
+        </div>
+        <p className={styles["heading"]}>Login</p>
+        <div className={styles["inputs-div"]}>
+          <input
+            onChange={onChange}
+            value={userLoginData.username}
+            name='username'
+            placeholder='username or email'
+            id='username'
+            type={"text"}
+          />
+          <input
+            onChange={onChange}
+            value={userLoginData.password}
+            name='password'
+            placeholder='password'
+            id='password'
+            type={"password"}
+          />
+          <button disabled={isDisabled}>submit</button>
+        </div>
 
-      <p>
-        Are you a new user?<Link to={"/signup"}>sign up</Link>
-      </p>
-    </form>
+        <p className={styles["p-link"]}>
+          New user?{" "}
+          <Link className={styles["link"]} to={"/signup"}>
+            sign up
+          </Link>
+        </p>
+      </form>
+    </div>
   );
 };
 
--- src/pages/SignUp/SignUp.js
+++ src/pages/SignUp/SignUp.js
@@ -1,13 +1,114 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
+import styles from "../Login/Login.module.css";
 
 const SignUp = (props) => {
+  const [userSignupData, setUserSignupData] = useState({
+    fullname: "",
+    email: "",
+    username: "",
+    password: "",
+    cpassword: "",
+  });
+  const [errMsg, setErrMsg] = useState("");
+
+  const [isDisabled, setIsDisabled] = useState(true);
+
+  const submitHandler = async (e) => {
+    e.preventDefault();
+
+    const dbUsername = "syed";
+    const dbPassword = "admin";
+
+    if (userSignupData.cpassword !== userSignupData.password) {
+      setErrMsg("password do not match.");
+    } else {
+      setErrMsg("");
+    }
+  };
+
+  const onChange = (e) => {
+    setUserSignupData((prev) => {
+      let helper = { ...prev };
+      helper[`${e.target.id}`] = e.target.value.replaceAll(" ", "");
+
+      if (
+        helper.fullname &&
+        helper.email &&
+        helper.username &&
+        helper.password &&
+        helper.cpassword
+      ) {
+        setIsDisabled(false);
+      } else {
+        setIsDisabled(true);
+      }
+
+      return helper;
+    });
+  };
+
   return (
-    <form>
-      <h4>Sign Up</h4>
-      <p>
-        Existing user?<Link to={"/login"}>login</Link>
-      </p>
-    </form>
+    <div className={styles["main"]}>
+      <form onSubmit={submitHandler} className={styles["form"]}>
+        <div className={styles["user-image"]}>
+          <div className={styles["head"]} />
+          <div className={styles["body"]} />
+        </div>
+        <p className={styles["heading"]}>Sign up</p>
+        <div className={styles["inputs-div"]}>
+          <input
+            onChange={onChange}
+            value={userSignupData.fullname}
+            name='fullname'
+            placeholder='full name'
+            id='fullname'
+            type={"text"}
+          />
+          <input
+            onChange={onChange}
+            value={userSignupData.email}
+            name='email'
+            placeholder='email'
+            id='email'
+            type={"email"}
+          />
+          <input
+            onChange={onChange}
+            value={userSignupData.username}
+            name='username'
+            placeholder='username'
+            id='username'
+            type={"text"}
+          />
+          <input
+            onChange={onChange}
+            value={userSignupData.password}
+            name='password'
+            placeholder='password'
+            id='password'
+            type={"password"}
+          />
+          <input
+            onChange={onChange}
+            value={userSignupData.cpassword}
+            name='cpassword'
+            placeholder='confirm password'
+            id='cpassword'
+            type={"password"}
+          />
+          <button disabled={isDisabled}>submit</button>
+          {errMsg}
+        </div>
+
+        <p className={styles["p-link"]}>
+          Existing user?{" "}
+          <Link className={styles["link"]} to={"/login"}>
+            login
+          </Link>
+        </p>
+      </form>
+    </div>
   );
 };
 
